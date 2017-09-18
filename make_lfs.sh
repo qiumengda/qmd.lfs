6fb321e10491b5a772868204507c5d52bf7b8b35
qiumengda <qmdlinux@163.com>
2017-09-18 16:34:44 +0800

    Update make_lfs.sh

--- make_lfs.sh
+++ make_lfs.sh
@@ -248,56 +248,6 @@ function make_rootfs()
 	sudo mknod -m 600 $ROOTFS/dev/console c 5 1
 	sudo mknod -m 666 $ROOTFS/dev/null c 1 3
 
-	#sudo bash -c "sed 's/tools/usr/' /tools/lib/libstdc++.la > usr/lib/libstdc++.la"
-
-	sudo bash -c "cat > etc/passwd << EOF
-root:x:0:0:root:/root:/bin/bash
-bin:x:1:1:bin:/dev/null:/bin/false
-daemon:x:6:6:Daemon User:/dev/null:/bin/false
-messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
-systemd-bus-proxy:x:72:72:systemd Bus Proxy:/:/bin/false
-systemd-journal-gateway:x:73:73:systemd Journal Gateway:/:/bin/false
-systemd-journal-remote:x:74:74:systemd Journal Remote:/:/bin/false
-systemd-journal-upload:x:75:75:systemd Journal Upload:/:/bin/false
-systemd-network:x:76:76:systemd Network Management:/:/bin/false
-systemd-resolve:x:77:77:systemd Resolver:/:/bin/false
-systemd-timesync:x:78:78:systemd Time Synchronization:/:/bin/false
-nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
-EOF"
-
-	sudo bash -c "cat > etc/group << EOF
-root:x:0:
-bin:x:1:daemon
-sys:x:2:
-kmem:x:3:
-tape:x:4:
-tty:x:5:
-daemon:x:6:
-floppy:x:7:
-disk:x:8:
-lp:x:9:
-dialout:x:10:
-audio:x:11:
-video:x:12:
-utmp:x:13:
-usb:x:14:
-cdrom:x:15:
-adm:x:16:
-messagebus:x:18:
-systemd-journal:x:23:
-input:x:24:
-mail:x:34:
-systemd-bus-proxy:x:72:
-systemd-journal-gateway:x:73:
-systemd-journal-remote:x:74:
-systemd-journal-upload:x:75:
-systemd-network:x:76:
-systemd-resolve:x:77:
-systemd-timesync:x:78:
-nogroup:x:99:
-users:x:999:
-EOF"
-
 	#exec tools/bin/bash --login +h
 
 	sudo touch var/log/{btmp,lastlog,wtmp}
@@ -2553,10 +2503,6 @@ function make_system_shadow()
 		echo fail; exit
 	fi
 	mv -v /usr/bin/passwd /bin
-	# Configure shadow
-	pwconv
-	grpconv
-	#passwd root
 	cd -
 }
 
@@ -4285,8 +4231,65 @@ function config_system()
 # End /etc/fstab
 EOF
 
+	filepath=/etc/passwd
+	echo "Create $filepath"
+	cat > $filepath << EOF
+root::0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/dev/null:/bin/false
+daemon:x:6:6:Daemon User:/dev/null:/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
+systemd-bus-proxy:x:72:72:systemd Bus Proxy:/:/bin/false
+systemd-journal-gateway:x:73:73:systemd Journal Gateway:/:/bin/false
+systemd-journal-remote:x:74:74:systemd Journal Remote:/:/bin/false
+systemd-journal-upload:x:75:75:systemd Journal Upload:/:/bin/false
+systemd-network:x:76:76:systemd Network Management:/:/bin/false
+systemd-resolve:x:77:77:systemd Resolver:/:/bin/false
+systemd-timesync:x:78:78:systemd Time Synchronization:/:/bin/false
+nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
+EOF
+
+	filepath=/etc/group
+	echo "Create $filepath"
+	cat > $filepath << EOF
+root:x:0:
+bin:x:1:daemon
+sys:x:2:
+kmem:x:3:
+tape:x:4:
+tty:x:5:
+daemon:x:6:
+floppy:x:7:
+disk:x:8:
+lp:x:9:
+dialout:x:10:
+audio:x:11:
+video:x:12:
+utmp:x:13:
+usb:x:14:
+cdrom:x:15:
+adm:x:16:
+messagebus:x:18:
+systemd-journal:x:23:
+input:x:24:
+mail:x:34:
+systemd-bus-proxy:x:72:
+systemd-journal-gateway:x:73:
+systemd-journal-remote:x:74:
+systemd-journal-upload:x:75:
+systemd-network:x:76:
+systemd-resolve:x:77:
+systemd-timesync:x:78:
+nogroup:x:99:
+users:x:999:
+EOF
+	# Configure shadow
+	pwconv
+	grpconv
+	# Passwd root
+	passwd root
+
 	filepath=/etc/profile
-	echo "Create bash configuration file $filepath"
+	echo "Create $filepath"
 	cat > $filepath << "EOF"
 # Begin /etc/profile
 
@@ -4298,7 +4301,7 @@ alias grep='grep --color=auto'
 EOF
 
 	filepath=/etc/vimrc
-	echo "Create vim configuration file $filepath"
+	echo "Create $filepath"
 	cat >> $filepath << "EOF"
 syntax on  
 set history=100  
@@ -4340,7 +4343,8 @@ function make_system_kernel()
 		echo fail; exit
 	fi
 	#make menuconfig
-	cp /build/kernel_config-3.19 .config
+	#cp /build/kernel_config-3.19 .config
+	cp /build/kernel_config-3.19.x .config
 	if [ $? != 0 ]; then
 		echo fail; exit
 	fi
@@ -4361,6 +4365,369 @@ function make_system_kernel()
 		cp -r Documentation/* /usr/share/doc/linux-3.19
 	fi
 	cd -
+}
+
+function make_system_cpio()
+{
+	app=cpio-2.12
+	tar=$SOURCE_TAR/$app.tar.bz2
+	src=$SYSTEM_SRC/$app
+	build=$SYSTEM_SRC/$app
+
+	if [ ! -d $src ]; then
+		tar -xvf $tar -C $SYSTEM_SRC
+	fi
+
+	cd $build
+	./configure --prefix=/usr \
+		--bindir=/bin \
+		--enable-mt   \
+		--with-rmt=/usr/libexec/rmt &&
+	if [ $? != 0 ]; then
+		echo fail; exit
+	fi
+	make
+	if [ $? != 0 ]; then
+		echo fail; exit
+	fi
+	if [ "$MAKE_CHECK" == "yes" ]; then
+		make check
+		if [ $? != 0 ]; then
+			echo fail; exit
+		fi
+	fi
+	if [ "$MAKE_DOC" == "yes" ]; then
+		makeinfo --html            -o doc/html      doc/cpio.texi &&
+		makeinfo --html --no-split -o doc/cpio.html doc/cpio.texi &&
+		makeinfo --plaintext       -o doc/cpio.txt  doc/cpio.texi
+		# texlive-20160523b
+		#make -C doc pdf &&
+		#make -C doc ps
+	fi
+	make install
+	if [ $? != 0 ]; then
+		echo fail; exit
+	fi
+	if [ "$MAKE_DOC" == "yes" ]; then
+		install -v -m755 -d /usr/share/doc/cpio-2.12/html &&
+		install -v -m644    doc/html/* \
+				    /usr/share/doc/cpio-2.12/html &&
+		install -v -m644    doc/cpio.{html,txt} \
+				    /usr/share/doc/cpio-2.12
+		install -v -m644    doc/cpio.{pdf,ps,dvi} \
+				    /usr/share/doc/cpio-2.12
+	fi
+	cd -
+}
+
+function make_system_initrd()
+{
+	make_system_cpio
+
+	cat > /sbin/mkinitramfs << "EOF"
+#!/bin/bash
+# This file based in part on the mkinitramfs script for the LFS LiveCD
+# written by Alexander E. Patrakov and Jeremy Huntwork.
+
+copy()
+{
+	local file
+
+	if [ "$2" == "lib" ]; then
+		file=$(PATH=/lib:/usr/lib type -p $1)
+	else
+		file=$(type -p $1)
+	fi
+
+  if [ -n $file ] ; then
+    cp $file $WDIR/$2
+  else
+    echo "Missing required file: $1 for directory $2"
+    rm -rf $WDIR
+    exit 1
+  fi
+}
+
+if [ -z $1 ] ; then
+  INITRAMFS_FILE=initrd.img-no-kmods
+else
+  KERNEL_VERSION=$1
+  INITRAMFS_FILE=initrd.img-$KERNEL_VERSION
+fi
+
+if [ -n "$KERNEL_VERSION" ] && [ ! -d "/lib/modules/$1" ] ; then
+  echo "No modules directory named $1"
+  exit 1
+fi
+
+printf "Creating $INITRAMFS_FILE... "
+
+binfiles="sh cat cp dd killall ls mkdir mknod mount "
+binfiles="$binfiles umount sed sleep ln rm uname"
+
+# Systemd installs udevadm in /bin. Other udev implementations have it in /sbin
+if [ -x /bin/udevadm ] ; then binfiles="$binfiles udevadm"; fi
+
+sbinfiles="modprobe blkid switch_root"
+
+#Optional files and locations
+for f in mdadm mdmon udevd udevadm; do
+  if [ -x /sbin/$f ] ; then sbinfiles="$sbinfiles $f"; fi
+done
+
+unsorted=$(mktemp /tmp/unsorted.XXXXXXXXXX)
+
+DATADIR=/usr/share/mkinitramfs
+INITIN=init.in
+
+# Create a temporary working directory
+WDIR=$(mktemp -d /tmp/initrd-work.XXXXXXXXXX)
+
+# Create base directory structure
+mkdir -p $WDIR/{bin,dev,lib/firmware,run,sbin,sys,proc}
+mkdir -p $WDIR/etc/{modprobe.d,udev/rules.d}
+touch $WDIR/etc/modprobe.d/modprobe.conf
+ln -s lib $WDIR/lib64
+
+# Create necessary device nodes
+mknod -m 640 $WDIR/dev/console c 5 1
+mknod -m 664 $WDIR/dev/null    c 1 3
+
+# Install the udev configuration files
+if [ -f /etc/udev/udev.conf ]; then
+  cp /etc/udev/udev.conf $WDIR/etc/udev/udev.conf
+fi
+
+for file in $(find /etc/udev/rules.d/ -type f) ; do
+  cp $file $WDIR/etc/udev/rules.d
+done
+
+# Install any firmware present
+cp -a /lib/firmware $WDIR/lib
+
+# Copy the RAID configuration file if present
+if [ -f /etc/mdadm.conf ] ; then
+  cp /etc/mdadm.conf $WDIR/etc
+fi
+
+# Install the init file
+install -m0755 $DATADIR/$INITIN $WDIR/init
+
+if [  -n "$KERNEL_VERSION" ] ; then
+  if [ -x /bin/kmod ] ; then
+    binfiles="$binfiles kmod"
+  else
+    binfiles="$binfiles lsmod"
+    sbinfiles="$sbinfiles insmod"
+  fi
+fi
+
+# Install basic binaries
+for f in $binfiles ; do
+  ldd /bin/$f | sed "s/\t//" | cut -d " " -f1 >> $unsorted
+  copy $f bin
+done
+
+# Add lvm if present
+if [ -x /sbin/lvm ] ; then sbinfiles="$sbinfiles lvm dmsetup"; fi
+
+for f in $sbinfiles ; do
+  ldd /sbin/$f | sed "s/\t//" | cut -d " " -f1 >> $unsorted
+  copy $f sbin
+done
+
+# Add udevd libraries if not in /sbin
+if [ -x /lib/udev/udevd ] ; then
+  ldd /lib/udev/udevd | sed "s/\t//" | cut -d " " -f1 >> $unsorted
+elif [ -x /lib/systemd/systemd-udevd ] ; then
+  ldd /lib/systemd/systemd-udevd | sed "s/\t//" | cut -d " " -f1 >> $unsorted
+fi
+
+# Add module symlinks if appropriate
+if [ -n "$KERNEL_VERSION" ] && [ -x /bin/kmod ] ; then
+  ln -s kmod $WDIR/bin/lsmod
+  ln -s kmod $WDIR/bin/insmod
+fi
+
+# Add lvm symlinks if appropriate
+# Also copy the lvm.conf file
+if  [ -x /sbin/lvm ] ; then
+  ln -s lvm $WDIR/sbin/lvchange
+  ln -s lvm $WDIR/sbin/lvrename
+  ln -s lvm $WDIR/sbin/lvextend
+  ln -s lvm $WDIR/sbin/lvcreate
+  ln -s lvm $WDIR/sbin/lvdisplay
+  ln -s lvm $WDIR/sbin/lvscan
+
+  ln -s lvm $WDIR/sbin/pvchange
+  ln -s lvm $WDIR/sbin/pvck
+  ln -s lvm $WDIR/sbin/pvcreate
+  ln -s lvm $WDIR/sbin/pvdisplay
+  ln -s lvm $WDIR/sbin/pvscan
+
+  ln -s lvm $WDIR/sbin/vgchange
+  ln -s lvm $WDIR/sbin/vgcreate
+  ln -s lvm $WDIR/sbin/vgscan
+  ln -s lvm $WDIR/sbin/vgrename
+  ln -s lvm $WDIR/sbin/vgck
+  # Conf file(s)
+  cp -a /etc/lvm $WDIR/etc
+fi
+
+# Install libraries
+sort $unsorted | uniq | while read library ; do
+  if [ "$library" == "linux-vdso.so.1" ] ||
+     [ "$library" == "linux-gate.so.1" ]; then
+    continue
+  fi
+
+  copy $library lib
+done
+
+if [ -d /lib/udev ]; then
+  cp -a /lib/udev $WDIR/lib
+fi
+if [ -d /lib/systemd ]; then
+  cp -a /lib/systemd $WDIR/lib
+fi
+
+# Install the kernel modules if requested
+if [ -n "$KERNEL_VERSION" ]; then
+  find                                                                        \
+     /lib/modules/$KERNEL_VERSION/kernel/{crypto,fs,lib}                      \
+     /lib/modules/$KERNEL_VERSION/kernel/drivers/{block,ata,md,firewire}      \
+     /lib/modules/$KERNEL_VERSION/kernel/drivers/{scsi,message,pcmcia,virtio} \
+     /lib/modules/$KERNEL_VERSION/kernel/drivers/usb/{host,storage}           \
+     -type f 2> /dev/null | cpio --make-directories -p --quiet $WDIR
+
+  cp /lib/modules/$KERNEL_VERSION/modules.{builtin,order}                     \
+            $WDIR/lib/modules/$KERNEL_VERSION
+
+  depmod -b $WDIR $KERNEL_VERSION
+fi
+
+( cd $WDIR ; find . | cpio -o -H newc --quiet | gzip -9 ) > $INITRAMFS_FILE
+
+# Remove the temporary directory and file
+rm -rf $WDIR $unsorted
+printf "done.\n"
+
+EOF
+
+	chmod 0755 /sbin/mkinitramfs
+
+mkdir -p /usr/share/mkinitramfs &&
+cat > /usr/share/mkinitramfs/init.in << "EOF"
+#!/bin/sh
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+export PATH
+
+problem()
+{
+   printf "Encountered a problem!\n\nDropping you to a shell.\n\n"
+   sh
+}
+
+no_device()
+{
+   printf "The device %s, which is supposed to contain the\n" $1
+   printf "root file system, does not exist.\n"
+   printf "Please fix this problem and exit this shell.\n\n"
+}
+
+no_mount()
+{
+   printf "Could not mount device %s\n" $1
+   printf "Sleeping forever. Please reboot and fix the kernel command line.\n\n"
+   printf "Maybe the device is formatted with an unsupported file system?\n\n"
+   printf "Or maybe filesystem type autodetection went wrong, in which case\n"
+   printf "you should add the rootfstype=... parameter to the kernel command line.\n\n"
+   printf "Available partitions:\n"
+}
+
+do_mount_root()
+{
+   mkdir /.root
+   [ -n "$rootflags" ] && rootflags="$rootflags,"
+   rootflags="$rootflags$ro"
+
+   case "$root" in
+      /dev/* ) device=$root ;;
+      UUID=* ) eval $root; device="/dev/disk/by-uuid/$UUID"  ;;
+      LABEL=*) eval $root; device="/dev/disk/by-label/$LABEL" ;;
+      ""     ) echo "No root device specified." ; problem    ;;
+   esac
+
+   while [ ! -b "$device" ] ; do
+       no_device $device
+       problem
+   done
+
+   if ! mount -n -t "$rootfstype" -o "$rootflags" "$device" /.root ; then
+       no_mount $device
+       cat /proc/partitions
+       while true ; do sleep 10000 ; done
+   else
+       echo "Successfully mounted device $root"
+   fi
+}
+
+init=/sbin/init
+root=
+rootdelay=
+rootfstype=auto
+ro="ro"
+rootflags=
+device=
+
+mount -n -t devtmpfs devtmpfs /dev
+mount -n -t proc     proc     /proc
+mount -n -t sysfs    sysfs    /sys
+mount -n -t tmpfs    tmpfs    /run
+
+read -r cmdline < /proc/cmdline
+
+for param in $cmdline ; do
+  case $param in
+    init=*      ) init=${param#init=}             ;;
+    root=*      ) root=${param#root=}             ;;
+    rootdelay=* ) rootdelay=${param#rootdelay=}   ;;
+    rootfstype=*) rootfstype=${param#rootfstype=} ;;
+    rootflags=* ) rootflags=${param#rootflags=}   ;;
+    ro          ) ro="ro"                         ;;
+    rw          ) ro="rw"                         ;;
+  esac
+done
+
+# udevd location depends on version
+if [ -x /sbin/udevd ]; then
+  UDEVD=/sbin/udevd
+elif [ -x /lib/udev/udevd ]; then
+  UDEVD=/lib/udev/udevd
+elif [ -x /lib/systemd/systemd-udevd ]; then
+  UDEVD=/lib/systemd/systemd-udevd
+else
+  echo "Cannot find udevd nor systemd-udevd"
+  problem
+fi
+
+${UDEVD} --daemon --resolve-names=never
+udevadm trigger
+udevadm settle
+
+if [ -f /etc/mdadm.conf ] ; then mdadm -As                       ; fi
+if [ -x /sbin/vgchange  ] ; then /sbin/vgchange -a y > /dev/null ; fi
+if [ -n "$rootdelay"    ] ; then sleep "$rootdelay"              ; fi
+
+do_mount_root
+
+killall -w ${UDEVD##*/}
+
+exec switch_root /.root "$init" "$@"
+
+EOF
+
 }
 
 function config_system_grub()
@@ -4466,6 +4833,7 @@ function make_system()
 	make_system_vim
 
 	make_system_kernel
+	make_system_initrd
 	config_system
 }
 
@@ -4541,7 +4909,10 @@ function main()
 		change_root $2
 		;;
 	"system")
-		make_system $2
+		#make_system $2
+#make_system_kernel
+#make_system_cpio
+make_system_initrd
 		;;
 	"vmdk")
 		make_vmdk $2
